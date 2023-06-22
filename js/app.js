619325a06824b609e90aa45fb0e8a2172059fa39
Jakub Gaj <gaju2001@o2.pl>
2023-06-22 18:50:39 +0200

    add option to change mark

--- js/app.js
+++ js/app.js
@@ -1,61 +1,55 @@
-const numberBtns = document.querySelectorAll(".calculator__btn--number")
-const markBtns = document.querySelectorAll(".calculator__btn--mark")
-const screen = document.querySelector(".screen__text")
-let flag = true
-let firstNumber = ""
-let secondNumber = ""
-let mark = ""
+const numberBtns = document.querySelectorAll(".calculator__btn--number");
+const markBtns = document.querySelectorAll(".calculator__btn--mark");
+const screen = document.querySelector(".screen__text");
+let flag = true;
+let firstNumber = "";
+let secondNumber = "";
+let mark = "";
 
-numberBtns.forEach(btn => btn.addEventListener("click",()=>{
-   if(flag){
-screen.textContent = ""
-   }
-screen.textContent += btn.textContent
-flag = false
-firstNumber += btn.textContent
-}))
-
-
-markBtns.forEach(btn => btn.addEventListener("click",()=>{
-   
-    if(firstNumber !== "" && secondNumber !== ""){
-        count()
-        mark = btn.textContent
-        firstNumber = screen.textContent
-        screen.textContent += mark
-        
-    } else if(firstNumber !== ""){
-    mark = btn.textContent
-    screen.textContent = firstNumber + mark
-   
-    } 
-         else
-{
-    mark = btn.textContent
-    screen.textContent = firstNumber + mark
+numberBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (flag) {
+      screen.textContent = "";
     }
-    secondNumber = firstNumber
-    firstNumber = "" 
-    console.log(mark) 
-    console.log(firstNumber)
-    console.log(secondNumber)
-}))
+    screen.textContent += btn.textContent;
+    flag = false;
+    firstNumber += btn.textContent;
+  })
+);
 
+markBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (firstNumber !== "" && secondNumber !== "") {
+      count();
+      mark = btn.textContent;
+      firstNumber = screen.textContent;
+      screen.textContent += mark;
+      secondNumber = firstNumber;
+      firstNumber = "";
+    } else if (firstNumber !== "") {
+      mark = btn.textContent;
+      screen.textContent = firstNumber + mark;
+      secondNumber = firstNumber;
+      firstNumber = "";
+    } else {
+      mark = btn.textContent;
+      screen.textContent = secondNumber + mark;
+    }
+  })
+);
 
+const count = () => {
+  if (mark === "+") {
+    screen.textContent = secondNumber * 1 + firstNumber * 1;
+  } else if (mark === "-") {
+    screen.textContent = secondNumber * 1 - firstNumber * 1;
+  } else if (mark === "/") {
+    screen.textContent = ((secondNumber * 1) / firstNumber) * 1;
+  } else if (mark === "*") {
+    screen.textContent = secondNumber * 1 * firstNumber * 1;
+  }
+};
 
-const count = ()=>{
-    if(mark === "+"){
-screen.textContent = secondNumber * 1 + firstNumber * 1
-    }
-    else if (mark === "-"){
-        screen.textContent = secondNumber * 1 - firstNumber * 1
-    }
-    else if (mark === "/"){
-        screen.textContent = secondNumber * 1 / firstNumber * 1
-    }
-    else if (mark === "*"){
-        screen.textContent = secondNumber * 1 * firstNumber * 1
-    }
-}
-
-document.querySelector(".calculator__btn--equal").addEventListener("click",count)
+document
+  .querySelector(".calculator__btn--equal")
+  .addEventListener("click", count);
